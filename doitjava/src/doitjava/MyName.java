5d70538cdd7d524a29c86aeac6beb4365c49694d
yn-14 <yn-14@LAPTOP-29BN5FPD>
2020-08-11 14:55:44 +0900

    pratice01~02_java

---
+++ doitjava/src/doitjava/MyName.java
@@ -0,0 +1,35 @@
+/**
+ * (asterisk)를 2개 사용한 주석은
+ * 도큐먼터리 주석으로 문서의 정보를
+ * 나타낼 때 사용한다
+ * 만든이 : 나
+ * 작성일 : 지금 
+ */
+
+
+//한 줄 주석
+//이것은 주석이 안니라서 예외를 발생시킨다.
+//주석은 컴파일할 때 삭제되어서 컴파일 된다.
+/*
+ 이것은 여러줄
+  	주석이므로
+ 	 여러줄 쓸 때
+  		사용한다
+ */
+package doitjava;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 내 이름은 휸이야");//이름
+		System.out.println("안물? ㅇㅋㅇㅋ");//두번째 줄
+		System.out.println("마침표 없으니까 출력 안됨");
+	}
+
+}
+/*
+ * 수업시간에 처음 작성한 코드입니다.
+ * 최초의 화면출력을 배웠습니다.
+ * 
+ */
