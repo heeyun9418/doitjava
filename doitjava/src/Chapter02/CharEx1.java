c1b8bdc8b8903c8932a373669f5f98837b740ec8
heeyun9418 <jjhhyy9418@gmail.com>
2020-08-14 16:19:13 +0900

    chapter02_java(정수 자료형)

---
+++ doitjava/src/Chapter02/CharEx1.java
@@ -0,0 +1,24 @@
+package Chapter02;
+
+public class CharEx1 {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+//		ch1 = 'BC' //char 자료형은 1개의 문자만 입력 가능하다
+		int num1 = ch1;
+		System.out.println(num1); //대문자A는 숫자65와 같다
+		
+		char ch2 = 66;
+		char ch3 = 67;
+		System.out.println(ch2);
+		System.out.println(ch3);
+		
+		char ch4 = 'a';
+		int num4 = ch4;
+		System.out.println(num4);
+		
+		System.out.println('a' - 'A');
+	}
+
+}
