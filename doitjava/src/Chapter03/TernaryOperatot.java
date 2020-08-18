9bbcd80bbfe2473c7fae2b4e86f563fc03932ca2
heeyun9418 <jjhhyy9418@gmail.com>
2020-08-18 15:07:51 +0900

    chapter03_java(조건 연산자)

---
+++ doitjava/src/Chapter03/TernaryOperatot.java
@@ -0,0 +1,22 @@
+package Chapter03;
+
+public class TernaryOperatot {
+
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge) ? 'T' : 'F';
+		
+		System.out.println(ch);
+		
+		int num = 15;
+		
+		boolean isEven;
+		isEven = (num % 2) == 0 ? true : false;
+		
+		System.out.println("Â¦¼öÀÎ°¡¿ä? " + (isEven? "Â¦¼ö" : "È¦¼ö"));
+	}
+
+}
