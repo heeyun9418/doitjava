ea6a6202484e7e77a862ea5f2d624b06b10d692e
heeyun9418 <jjhhyy9418@gmail.com>
2020-09-07 15:23:01 +0900

    practice01~02_java

--- doitjava/src/doitjava/GitTest2.java
+++ doitjava/src/doitjava/GitTest2.java
@@ -4,7 +4,7 @@ public class GitTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2nd commit TEST")
+		System.out.println("2nd commit TEST");
 	}
 
 }
